4f60cfbb3e8e3b9df84e54f15fcd26617046e17f
Menno Tempelaar <menno@made-foryou.nl>
2024-01-13 17:15:52 +0100

    add the usesPackage method

--- src/Packages.php
+++ src/Packages.php
@@ -2,7 +2,19 @@
 
 namespace MadeForYou\Helpers;
 
+use Illuminate\Container\Container;
+
 class Packages
 {
+    const PACKAGES = [
+        'category' => 'MadeForYou\\Categories\\FilamentCategoriesPlugin',
+        'news' => 'MadeForYou\\News\\NewsPlugin',
+    ];
 
+    public function usesPackage(string $name): bool
+    {
+        return Container::getInstance()->resolved(
+            self::PACKAGES[$name]
+        );
+    }
 }
