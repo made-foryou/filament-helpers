9d25fd5cf122a252eb5134f56315be3d763f1613
Menno Tempelaar <menno@made-foryou.nl>
2024-01-13 17:17:47 +0100

    add docs for methods within the facade.

--- src/Facades/Packages.php
+++ src/Facades/Packages.php
@@ -4,6 +4,9 @@ namespace MadeForYou\Helpers\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method bool usesPackages(string $name)
+ */
 class Packages extends Facade
 {
     protected static function getFacadeAccessor(): string
