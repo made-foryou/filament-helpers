82b8ec93e1d19afba414a6e45021c5d084a7c713
Menno Tempelaar <menno@made-foryou.nl>
2024-01-19 14:13:04 +0100

    feat: make the uses method static inside the docblock

--- src/Facades/Packages.php
+++ src/Facades/Packages.php
@@ -5,7 +5,7 @@ namespace MadeForYou\Helpers\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method bool uses(string $name)
+ * @method static bool uses(string $name)t
  */
 class Packages extends Facade
 {
